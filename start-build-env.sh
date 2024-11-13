47c85821038324dae09e6f6b09c0dcd5b2224100
m.desoutter <m.desoutter@criteo.com>
2024-11-13 14:35:56 +0100

    build: add docs and details

--- start-build-env.sh
+++ start-build-env.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-docker build -t vertica-builder:local .
+_b_env=${1:-centos8.Dockerfile}
+errout() {
+  _r=$1
+  shift 1;
+  echo $@;
+  exit $_r;
+}
+BUILDER_IMAGE="vertica-builder-hyperloglog:local"
+BUILDER_INSTANCE_NAME="vbuilder-hyperloglog"
+docker build -f ${_b_env} -t ${BUILDER_IMAGE} .
+test -f /opt/vertica/sdk/include/BuildInfo.h || errout 30 "Vertica SDK is missing. Put it in /opt/vertica/sdk so that /opt/vertica/sdk/include/BuildInfo.h exists"
 
 rm -rf build
 mkdir -p build
-docker kill vbuilder-hyperloglog || true
-docker rm vbuilder-hyperloglog || true
-docker run --rm --name vbuilder-hyperloglog -v=$PWD/SOURCES:/sources  -v=$PWD/build:/build -v=/opt/vertica/sdk:/opt/vertica/sdk  -d vertica-builder:local sleep 3600
-echo "Run docker exec -ti vbuilder-hyperloglog bash # now"
+docker kill ${BUILDER_INSTANCE_NAME} || true
+sleep 1
+docker rm ${BUILDER_INSTANCE_NAME} || true
+sleep 1
+docker run --rm --name ${BUILDER_INSTANCE_NAME} -v=$PWD/SOURCES:/sources -v=$PWD/build:/build -v=/opt/vertica/sdk:/opt/vertica/sdk  -d $BUILDER_IMAGE sleep 3600
+echo "Run docker exec -ti ${BUILDER_INSTANCE_NAME} bash # now"
+echo "Then, you may run the following to build"
+echo "cmake /sources; make clean; make"
